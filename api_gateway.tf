eb2ec8f84993417ab0546a67b8745b6ce16c0709
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2024-08-13 16:47:18 -0400

    back to broken

--- api_gateway.tf
+++ api_gateway.tf
@@ -19,7 +19,7 @@ resource "aws_api_gateway_resource" "this" {
 resource "aws_api_gateway_method" "this" {
   rest_api_id   = aws_api_gateway_rest_api.this.id
   resource_id   = aws_api_gateway_resource.this.id
-  http_method   = "POST"
+  http_method   = "GET"
   authorization = "NONE"
 }
 
@@ -27,7 +27,7 @@ resource "aws_api_gateway_integration" "this" {
   rest_api_id             = aws_api_gateway_rest_api.this.id
   resource_id             = aws_api_gateway_resource.this.id
   http_method             = aws_api_gateway_method.this.http_method
-  integration_http_method = "POST"
+  integration_http_method = "GET"
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.this.invoke_arn
 }
