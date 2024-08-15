4e4d3528ec61063a3f99dee93ba0bf1bb727a89e
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2024-08-15 16:25:41 -0400

    moar outputs

--- output.tf
+++ output.tf
@@ -13,7 +13,11 @@ output "lambda_invoke_arn" {
 }
 
 output "lambda_function_name" {
-  description = "The Invoke ARN of the Lambda function."
+  description = "The name of the lambda function."
   value       = aws_lambda_function.this.function_name
 }
 
+output "github_repository_url" {
+  description = "The URL of the Github repository."
+  value       = github_repository.code.html_url
+}
